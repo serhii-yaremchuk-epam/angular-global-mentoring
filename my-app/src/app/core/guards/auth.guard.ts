e3677f64e57544adeabadb83dff37153f991a9bb
Serhii Yaremchuk <Serhii_Yaremchuk@epam.com>
2020-11-24 14:54:57 +0200

    task 5 refactoring

--- my-app/src/app/core/guards/auth.guard.ts
+++ my-app/src/app/core/guards/auth.guard.ts
@@ -9,7 +9,7 @@ import { AuthService } from '../services/auth.service';
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) { }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     if (this.authService.isAuthenticated()) {
       return true;
     }
--- my-app/src/app/core/ui/header/header.component.ts
+++ my-app/src/app/core/ui/header/header.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { User } from '../../../shared/models/user.model';
 
 @Component({
   selector: 'cp-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  public user!: User;
 
   constructor(public authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.user = this.authService.getUserInfo();
+  }
 }
